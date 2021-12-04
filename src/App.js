e9b950e99e7dfdc60e7f70162a624f75df48bfa2
smajaas <smajaas@gmail.com>
2021-12-04 17:38:01 +0530

    recipe-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  //const recipe = "Hyderabad Mutton Briyani";
+  const recipes = [
+    {
+      poster:
+        "https://5.imimg.com/data5/BU/HQ/SL/SELLER-29361735/img-8992-jpg-500x500.JPG",
+      name: "Mutton Briyani",
+    },
+
+    {
+      poster:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp-5XIhtQ7MkrmeoZYKRc4H8lb7xDQDjqUcQ&usqp=CAU",
+      name: "Paneer Butter Masala",
+    },
+    {
+      poster:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKJ1vQx7F1RaEoV9iET6GBTrRQ4F0FoEK9oA&usqp=CAU",
+      name: "Al-Baik Chicken",
+    },
+    {
+      poster:
+        "https://image.eatencdn.com/image/ed415320-7612-4be3-8afa-be64f566c2d4/image.jpg",
+      name: "Mutton Mandhi",
+    },
+    {
+      poster:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsSZO0ELaLgsdSqdOCzABzQn5aRsFIN9HfFA&usqp=CAU",
+      name: "Pomfret Tandoori",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="recipe-list-container">
+        {recipes.map((recipe, index) => (
+          <div key={index} className="recipe-container">
+            <img
+              className="recipe-poster"
+              src={recipe.poster}
+              alt={recipe.name}
+            />
+            <p className="recipe-name">{recipe.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
+//List only the recipe name
 export default App;
